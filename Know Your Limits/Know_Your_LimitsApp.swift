7f7601bb1290165b68bed3df5006c537b14619ce
Andrew <24258342+acodeguy@users.noreply.github.com>
2021-01-31 15:16:36 +0000

    Initial Commit

---
+++ Know Your Limits/Know_Your_LimitsApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct Know_Your_LimitsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Know Your LimitsTests/Know_Your_LimitsTests.swift
@@ -0,0 +1,4 @@
+import XCTest
+@testable import Know_Your_Limits
+
+class Know_Your_LimitsTests: XCTestCase { }
